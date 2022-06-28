cf9f573cc14e9162c54fd2d156a16793f2953abf
Varun Tanna <varun_tanna@ymail.com>
2022-06-28 18:16:53 -0400

    set up start of index.js

--- models/index.js
+++ models/index.js
@@ -3,10 +3,18 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { hasMany } = require('./Category');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category,hasMany(Product, {
+  foreignKey: 'category_id',
+
+});
 
 // Products belongToMany Tags (through ProductTag)
 
